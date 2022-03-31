8b9a90cb7333d45b4d09a1df409c13e006cf171a
ElenaMares0629 <102752039+ElenaMares0629@users.noreply.github.com>
2022-03-31 17:06:44 +0100

    initial commit

---
+++ HaikuReader.js
@@ -0,0 +1,149 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk-core');
+
+const GetNewFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest');
+  },
+  handle(handlerInput) {
+    const factArr = data;
+    const factIndex = Math.floor(Math.random() * factArr.length);
+    const randomFact = factArr[factIndex];
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .withSimpleCard(SKILL_NAME, randomFact)
+      .getResponse();
+  },
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(STOP_MESSAGE)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, an error occurred.')
+      .reprompt('Sorry, an error occurred.')
+      .getResponse();
+  },
+};
+
+const SKILL_NAME = 'Haiku Reader';
+const GET_FACT_MESSAGE = 'I have a haiku for you: ';
+const HELP_MESSAGE = 'You can say tell me a haiku...  or, you can say stop... What can I help you with?';
+const HELP_REPROMPT = 'You can ask me to tell a haiku or exit to stop. What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+const data = [
+  `Berlin is a place.
+  Full of historical stuff.
+  Come and visit us`,
+
+  `The last page is full.
+  Scribbled notes and reminders.
+  Goodbye dear notebook`,
+  
+  `Barrels are out there.
+  Big blue deep dark crashing waves.
+  Surfing is my dream`,
+  
+  `I wanna rip.
+  Ride the edge and cut the lip.
+  Balance and nature`,
+  
+  `Surfing emerald waves.
+  Gently cleanse vivid coral.
+  Tropical rainbow`,
+  
+  `It's been a rough day.
+  Catching a few waves heals me.
+  Thanks lord for the rest`,
+  
+  `The beach beckons.
+  Blazing blue oceans roar.
+  An invitation to surf`,
+  
+  `Surfing is skill.
+  Using power of water.
+  Riding on a board`,
+  
+  `Home by the ocean.
+  Where the waves keep rolling in.
+  Beauty to the max`,
+  
+  `The next wave to ride.
+  A decision must be made.
+  How to challenge it`,
+  
+  `The thrill of surfing.
+  Oh to ride that curl once more.
+  Only in my dreams`,
+  
+  `Surfer’s watch for swells.
+  There are large walls of water.
+  Breaking into waves`,
+
+  `The alarm goes off.
+  Blaring siren of my work.
+  I need some coffee.`
+];
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetNewFactHandler,
+    HelpHandler,
+    ExitHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
